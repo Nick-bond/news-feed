ca5ff38df90fd631d5d2ba1913110fa61de14f4a
nick-bond <n.jsdevelop@gmail.com>
2020-10-05 22:20:48 +0300

    update for build

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ app.use('/api/', require('./service/routes/post.routes'));
 app.use('/api/', require('./service/routes/get.routes'));
 
 if(process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname, '../', 'build')));
+  app.use('/', express.static(path.join(__dirname, '../', 'app/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../', 'app/build', 'app/index.html'));
   });
 }
 
