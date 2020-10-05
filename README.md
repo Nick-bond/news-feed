05305cdad446b4cc43ee26eea24acf35dd3b6498
nick-bond <n.jsdevelop@gmail.com>
2020-10-05 22:33:50 +0300

    update readme and configure api for correct render

--- README.md
+++ README.md
@@ -6,3 +6,22 @@ https://news-feed1.herokuapp.com/
 #### For local development run 
 
 `npm run dev`
+
+
+##### NOTE
+for local development need replace 
+`app/src/constants` 
+from 
+```
+  API_POSTS: '/api/posts',
+  API_ADD: '/api/post/create',
+  API_REMOVE: '/api/post/remove',
+```
+
+too 
+
+```
+  API_POSTS: 'http://localhost:5000/api/posts',
+  API_ADD: 'http://localhost:5000/api/post/create',
+  API_REMOVE: 'http://localhost:5000/api/post/remove',
+```
--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const path = require('path');
 
 const { MONGO_URI } = constants;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json({extended: true}));
@@ -14,9 +14,9 @@ app.use('/api/', require('./service/routes/post.routes'));
 app.use('/api/', require('./service/routes/get.routes'));
 
 if(process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname, '../', 'app/build')));
+  app.use('/', express.static(path.join(__dirname, 'app', 'build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../', 'app/build', 'app/index.html'));
+    res.sendFile(path.join(__dirname, 'app', 'build', 'index.html'));
   });
 }
 
