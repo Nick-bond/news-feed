b9cf1b0924aff5963ad62966c3a4dfa35009ed5d
nick-bond <n.jsdevelop@gmail.com>
2020-10-05 21:45:06 +0300

    update ports and api urls

--- app/src/constants/index.js
+++ app/src/constants/index.js
@@ -1,5 +1,5 @@
 export default {
-  API_POSTS: 'http://localhost:5000/api/posts',
-  API_ADD: 'http://localhost:5000/api/post/create',
-  API_REMOVE: 'http://localhost:5000/api/post/remove',
+  API_POSTS: '/api/posts',
+  API_ADD: '/api/post/create',
+  API_REMOVE: '/api/post/remove',
 };
